ffd10fbb25445dd82bb926201b9f223e2ec55e71
Andre Baumeier <hallo@andre-baumeier.de>
2016-11-22 15:59:15 +0100

    Update VotingApiWidget.php
    removing unused code, and enabled form label for voting status - this is really needed to know what those open / close buttons are for.

--- src/Plugin/Field/FieldWidget/VotingApiWidget.php
+++ src/Plugin/Field/FieldWidget/VotingApiWidget.php
@@ -22,44 +22,10 @@ class VotingApiWidget extends WidgetBase {
   /**
    * {@inheritdoc}
    */
-  public static function defaultSettings() {
-    return [
-      'size' => 60,
-      'placeholder' => '',
-    ] + parent::defaultSettings();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsForm(array $form, FormStateInterface $form_state) {
-    $elements = [];
-
-    return $elements;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsSummary() {
-    $summary = [];
-
-    $summary[] = t('Textfield size: @size', ['@size' => $this->getSetting('size')]);
-    if (!empty($this->getSetting('placeholder'))) {
-      $summary[] = t('Placeholder: @placeholder', ['@placeholder' => $this->getSetting('placeholder')]);
-    }
-
-    return $summary;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $element['status'] = array(
       '#type' => 'radios',
       '#title' => t('Votes'),
-      '#title_display' => 'invisible',
       '#default_value' => 1,
       '#options' => array(
         1 => t('Open'),
