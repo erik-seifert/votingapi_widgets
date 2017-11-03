9a95eff7280bb4f33891dd5b59457d5c0d49607f
Erik Seifert <erik.seifert@b-connect.de>
2017-11-03 16:12:14 +0100

    Use correct service implementation

--- src/VotingApiLoader.php
+++ src/VotingApiLoader.php
@@ -2,19 +2,29 @@
 
 namespace Drupal\votingapi_widgets;
 
+use Drupal\votingapi_widgets\Plugin\VotingApiWidgetManager;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
 /**
  * Implements lazy loading.
  */
 class VotingApiLoader {
 
+  protected $manager;
+  protected $entityTypeManager;
+
+  public function __construct(VotingApiWidgetManager $manager, EntityTypeManagerInterface $entity_type_manager) {
+    $this->manager = $manager;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
   /**
    * Build rate form.
    */
   public function buildForm($plugin_id, $entity_type, $entity_bundle, $entity_id, $vote_type, $field_name, $style, $show_results, $read_only, $show_own_vote) {
-    $manager = \Drupal::service('plugin.manager.voting_api_widget.processor');
-    $definitions = $manager->getDefinitions();
-    $entity = \Drupal::service('entity_type.manager')->getStorage($entity_type)->load($entity_id);
-    $plugin = $manager->createInstance($plugin_id, $definitions[$plugin_id]);
+    $definitions = $this->manager->getDefinitions();
+    $entity = $this->entityTypeManager->getStorage($entity_type)->load($entity_id);
+    $plugin = $this->manager->createInstance($plugin_id, $definitions[$plugin_id]);
     $fieldDefinition = $entity->{$field_name}->getFieldDefinition();
     if ($fieldDefinition->get('status') != 1) {
       $read_only = TRUE;
